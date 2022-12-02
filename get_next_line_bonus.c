a697e03a7154165e7c4a2cf9a3e289190f348cb5
devways <thomas.loyan.dev@gmail.com>
2022-12-02 20:55:53 +0100

    bonus: use an array of linked list (could also use only linked list without array but not really usefull and perf are not better)

---
+++ get_next_line_bonus.c
@@ -0,0 +1,125 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thloyan <thloyan@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/02 16:18:05 by thloyan           #+#    #+#             */
+/*   Updated: 2022/12/02 18:42:51 by thloyan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+int	read_and_stash(t_list **lst, int fd)
+{
+	char	*buffer;
+	int		readed;
+
+	buffer = malloc((BUFFER_SIZE + 1) * sizeof(*buffer));
+	if (!buffer)
+		return (-1);
+	buffer[0] = 0;
+	readed = 1;
+	while (readed && !ft_strchr(buffer, '\n'))
+	{
+		readed = read(fd, buffer, BUFFER_SIZE);
+		if (readed == -1)
+			return (free(buffer), -1);
+		buffer[readed] = 0;
+		if (ft_lstnew_gnl(&*lst, buffer) == NULL)
+			return (free(buffer), -1);
+	}
+	return (free(buffer), 0);
+}
+
+size_t	compute_line_len(t_list *lst)
+{
+	size_t		len;
+	size_t		i;
+
+	len = 0;
+	while (lst)
+	{
+		i = 0;
+		while (lst->content[i] != 0 && lst->content[i] != '\n')
+			i++;
+		len = len + i;
+		if (lst->content[i] == '\n' && len++)
+			break ;
+		lst = lst->next;
+	}
+	return (len);
+}
+
+void	create_line(t_list	*lst, char **line)
+{
+	size_t		len;
+	size_t		i;
+
+	len = 0;
+	while (lst)
+	{
+		i = 0;
+		while (lst->content[i] != 0 && lst->content[i] != '\n')
+			(*line)[len++] = lst->content[i++];
+		if (lst->content[i] == '\n')
+		{
+			(*line)[len++] = lst->content[i++];
+			break ;
+		}
+		lst = lst->next;
+	}
+	(*line)[len] = 0;
+	if (!ft_strlen(*line))
+	{
+		free(*line);
+		*line = NULL;
+	}
+}
+
+void	update_stash(t_list **lst)
+{
+	char		*str;
+	size_t		i;
+	size_t		n;
+	t_list		*tmp;
+
+	while (*lst && !ft_strchr((*lst)->content, '\n'))
+	{
+		tmp = (*lst)->next;
+		free((*lst)->content);
+		free(*lst);
+		*lst = tmp;
+	}
+	if (!*lst)
+		return ;
+	str = ft_strchr((*lst)->content, '\n') + 1;
+	n = ft_strlen(str);
+	i = 0;
+	while (n--)
+	{
+		(*lst)->content[i] = str[i];
+		i++;
+	}
+	(*lst)->content[i] = 0;
+}
+
+char	*get_next_line(int fd)
+{
+	static t_list	*lsts[FT_FD_MAX] = {};
+	char			*line;
+
+	line = NULL;
+	if (fd > FT_FD_MAX || read(fd, 0, 0) == -1 || BUFFER_SIZE <= 0)
+		return (ft_lstclear(lsts, &free), NULL);
+	if (read_and_stash(&lsts[fd], fd) == -1)
+		return (ft_lstclear(lsts, &free), NULL);
+	line = malloc((compute_line_len(lsts[fd]) + 1) * sizeof(*line));
+	if (!line)
+		return (ft_lstclear(lsts, &free), NULL);
+	create_line(lsts[fd], &line);
+	update_stash(&lsts[fd]);
+	return (line);
+}
